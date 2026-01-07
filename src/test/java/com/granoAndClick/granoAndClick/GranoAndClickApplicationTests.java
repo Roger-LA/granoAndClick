5a02687c5538dde5620b494ec0158adfd733f538
Elr1ck13 <renewcod13@outlook.com>
2026-01-07 14:12:43 -0600

    estructura inicial

---
+++ src/test/java/com/granoAndClick/granoAndClick/GranoAndClickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granoAndClick.granoAndClick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranoAndClickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
