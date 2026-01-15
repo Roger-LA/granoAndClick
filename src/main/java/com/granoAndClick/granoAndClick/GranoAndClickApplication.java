be3eaaddca761fe7951c0dc596b6872c8fcea93f
A. Emmanuel Aguilar V. <emmanuel.agva@gmail.com>
2026-01-15 11:08:24 -0600

    Se modifica el archivo aplicacion para escanear los paquetes antes de ejecutar, el archivo TiposUsuarios para validacion de usuarios creados anteriormente

--- src/main/java/com/granoAndClick/granoAndClick/GranoAndClickApplication.java
+++ src/main/java/com/granoAndClick/granoAndClick/GranoAndClickApplication.java
@@ -3,7 +3,7 @@ package com.granoAndClick.granoAndClick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.granoAndClick.granoAndClick")
 public class GranoAndClickApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/granoAndClick/granoAndClick/config/TiposUsuariosInitializer.java
+++ src/main/java/com/granoAndClick/granoAndClick/config/TiposUsuariosInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.granoAndClick.granoAndClick.model.TiposUsuarios;
 import com.granoAndClick.granoAndClick.repository.TiposUsuariosRepository;
@@ -13,16 +14,20 @@ import com.granoAndClick.granoAndClick.repository.TiposUsuariosRepository;
 @Order(1)
 public class TiposUsuariosInitializer {
 
+    @Autowired
+    private TiposUsuariosRepository tiposRep;
+
     @Bean
-    CommandLineRunner initTiposUsuarios(TiposUsuariosRepository tiposRep) {
+    CommandLineRunner initTiposUsuarios() {
         return args -> {
-            // Cargar variables desde .env
+
             Dotenv dotenv = Dotenv.load();
 
             String tipoUsuarioNombre1 = dotenv.get("TYPE_USER_DEFAULT_NOMBRE_1");
             String tipoUsuarioDescripcion1 = dotenv.get("TYPE_USER_DEFAULT_DESCRIPCION_1");
             String tipoUsuarioNombre2 = dotenv.get("TYPE_USER_DEFAULT_NOMBRE_2");
             String tipoUsuarioDescripcion2 = dotenv.get("TYPE_USER_DEFAULT_DESCRIPCION_2");
+
             if (tiposRep.count() == 0) {
                 TiposUsuarios admin = new TiposUsuarios(tipoUsuarioNombre1, tipoUsuarioDescripcion1);
                 TiposUsuarios user = new TiposUsuarios(tipoUsuarioNombre2, tipoUsuarioDescripcion2);
